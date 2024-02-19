4394a21b14a791f6e94843f022dce222e28322c0
Efmprof <evseev-2003@ya.ru>
2024-02-19 23:49:40 +0300

    Refactored

--- README.md
+++ README.md
@@ -5,11 +5,11 @@
 Приложенька которая дергает новости по апишке и красиво их показывает. Есть `freezed`,
 есть `dotenv` (свой токен дарю общественности). Все основные пункты выполнены.
 
+## APK
+
+А он [тут](build/app/outputs/apk/release/app-release.apk).
+
 ## Скриншотики
 
 ![Главная страница](etc/images/home_page.png)
 ![Страница новости](etc/images/detail_page.png)
-
-## APK
-
-А он [тут](build/app/outputs/apk/release/app-release.apk).
--- lib/models/news_article.dart
+++ lib/models/news_article.dart
@@ -1,13 +1,13 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
-part 'news_article.freezed.dart';
-part 'news_article.g.dart';
 
+part 'news_article.freezed.dart';
+
+part 'news_article.g.dart';
 
 @freezed
 class NewsArticle with _$NewsArticle {
   const factory NewsArticle({
-    // required NewsSource? source,
     required String? author,
     required String? title,
     required String? description,
@@ -20,15 +20,3 @@ class NewsArticle with _$NewsArticle {
   factory NewsArticle.fromJson(Map<String, Object?> json) =>
       _$NewsArticleFromJson(json);
 }
-
-//
-// class NewsSource {
-//
-//   const factory NewsSource({
-//     required String id,
-//     required String name,
-//   }) = _NewsSource;
-//
-//   factory NewsSource.fromJson(Map<String, Object?> json) =>
-//       _$NewsSourceFromJson(json);
-// }
